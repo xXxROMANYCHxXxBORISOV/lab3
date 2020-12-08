72d1b49576491214b1ced7bf11f7cd46372075be
Romanych <yurytimon21@gmail.com>
2020-12-08 16:43:50 +0300

    GornerTableModel is done

--- src/bsu/rfe/java/group6/lab3/Timonovich/A7/GornerTableModel.java
+++ src/bsu/rfe/java/group6/lab3/Timonovich/A7/GornerTableModel.java
@@ -1,18 +1,22 @@
 package bsu.rfe.java.group6.lab3.Timonovich.A7;
 
 import javax.swing.table.AbstractTableModel;
-public class GornerTableModel extends AbstractTableModel {
+public class GornerTableModel extends AbstractTableModel	{
+
     private Double[] coefficients;
     private Double from;
     private Double to;
     private Double step;
-    public GornerTableModel(Double from, Double to, Double step, Double[]
-            coefficients) {
+    private double result[] = new double[3];
+
+
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients)	{
+        this.coefficients = coefficients;
         this.from = from;
         this.to = to;
         this.step = step;
-        this.coefficients = coefficients;
     }
+
     public Double getFrom() {
         return from;
     }
@@ -22,30 +26,62 @@ public class GornerTableModel extends AbstractTableModel {
     public Double getStep() {
         return step;
     }
-    @Override
+
     public int getColumnCount() {
-// TODO Auto-generated method stub
-        return 0;
+        return 4;
     }
-    @Override
+
     public int getRowCount() {
-// TODO Auto-generated method stub
-        return 0;
+        return new Double(Math.ceil((to-from)/step)).intValue()+1;
     }
-    @Override
-    public Object getValueAt(int arg0, int arg1) {
-// TODO Auto-generated method stub
-        return null;
+
+    public Object getValueAt(int row, int col) 	{
+
+        double x = from + step*row;
+        switch (col){
+            case 0:
+                return x;
+            case 1:
+            {
+                result[0] = 0.0;
+                for(int i = 0; i < coefficients.length; i++){
+                    result[0] += Math.pow(x, coefficients.length-1-i)*coefficients[i];
+                }
+                return result[0];
+            }
+            case 2:
+            {
+                result[1] = 0.0;
+                int p = coefficients.length-1;
+                for(int i = 0; i < coefficients.length; i++){
+                    result[1] += Math.pow(x, coefficients.length-1-i)*coefficients[p--];
+                }
+                return result[1];
+            }
+            default:
+                return result[2] = result[1] - result[0];
+        }
     }
-    @Override
-    public Class<?> getColumnClass(int arg0) {
-// TODO Auto-generated method stub
-        return super.getColumnClass(arg0);
+
+
+    public Class<?> getColumnClass(int col) {
+        return Double.class;
     }
-    @Override
-    public String getColumnName(int arg0) {
-// TODO Auto-generated method stub
-        return super.getColumnName(arg0);
+
+
+    public String getColumnName(int col) {
+
+        switch (col){
+            case 0:
+                return "X";
+            case 1:
+                return "Значение многочлена";
+
+            default:
+                return "Целая часть чѐтная";
+        }
     }
 }
+
+
 
